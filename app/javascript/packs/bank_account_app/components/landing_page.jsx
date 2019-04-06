5d8afdb4a05c3fbe469f25a352dcdd4ef2f4992b
Pete Jabbour <petejabbour1@gmail.com>
2019-04-06 18:45:43 -0500

    LandingPage component
    * imports the styles/app.scss, included bootstrap classes (to be added later)

---
+++ app/javascript/packs/bank_account_app/components/landing_page.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../assets/styles/app'
+
+class LandingPage extends React.Component {
+  render() {
+    return(
+      <div className="landing-page">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3">
+              Hello World
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default LandingPage
