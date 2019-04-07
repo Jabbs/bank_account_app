31b4ed56070b9035fe928218867b8c4be92bf7cc
Pete Jabbour <petejabbour1@gmail.com>
2019-04-07 06:47:30 -0500

    BankAccountController actions
    * show, new, edit, create, update, and destroy actions

--- app/controllers/bank_accounts_controller.rb
+++ app/controllers/bank_accounts_controller.rb
@@ -1,11 +1,68 @@
 class BankAccountsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_bank_account, only: [:show, :edit, :update, :destroy]
 
+  # GET /bank_accounts
+  # GET /bank_accounts.json
   def index
     render json: collection, each_serializer: BankAccountSerializer, status: :ok
   end
 
+  # GET /bank_accounts/1
+  # GET /bank_accounts/1.json
+  def show
+    render json: BankAccountSerializer.new(@bank_account)
+  end
+
+  # GET /bank_accounts/new
+  def new
+    @bank_account = BankAccount.new
+  end
+
+  # GET /bank_accounts/1/edit
+  def edit
+  end
+
+  # POST /bank_accounts
+  # POST /bank_accounts.json
+  def create
+    @bank_account = BankAccount.new(bank_account_params)
+
+    if @bank_account.save
+      render json: @bank_account, status: :created
+    else
+      render json: @bank_account.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /bank_accounts/1
+  # PATCH/PUT /bank_accounts/1.json
+  def update
+    if @bank_account.update(bank_account_params)
+      render json: @bank_account, status: :ok
+    else
+      render json: @bank_account.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /bank_accounts/1
+  # DELETE /bank_accounts/1.json
+  def destroy
+    @bank_account.destroy
+    head :no_content
+  end
+
   private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_bank_account
+    @bank_account = BankAccount.find_by_id(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def bank_account_params
+    params.require(:bank_account).permit(:name)
+  end
 
   # TODO: only show those for a particular user, perhaps w pundit
   def collection
